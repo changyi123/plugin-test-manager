2565b3ccb0c7c65428b6fd0fcc64ca1183b7acbc
zbl91555 <zbl91555@163.com>
2021-12-07 16:00:33 +0800

    fix: fix bug

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -6,8 +6,7 @@ export const Item = Parse.Object.extend('Item');
 export const Workspace = Parse.Object.extend('Workspace');
 
 export const Test = Parse.Object.extend(`${appKey}_Test`);
+export const TestStep = Parse.Object.extend(`${appKey}_TestStep`);
 export const Repository = Parse.Object.extend(`${appKey}_Repository`);
 export const TestConfig = Parse.Object.extend(`${appKey}_TestConfig`);
 export const TestExecution = Parse.Object.extend(`${appKey}_TestExecution`);
-export const TestStep = Parse.Object.extend(`${appKey}_TestStep`);
-export const Test = Parse.Object.extend(`${appKey}_Test`);
