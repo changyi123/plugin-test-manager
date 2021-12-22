2791d3dc87ab108c42193a6767b7c9f4f0e08427
zbl91555 <zbl91555@163.com>
2021-12-22 13:25:36 +0800

    test: add test dev

--- src/devEnv.ts
+++ src/devEnv.ts
@@ -1,4 +1,4 @@
 // 事项id
-export const PanelItemId = 'DF3PYNw3bZ';
+export const PanelItemId = 'RQLOlTgGDK';
 // 空间 key
 export const WorkspaceKey = 'TEST_MANAGE_1';
--- src/pages/demo/Demo.tsx
+++ src/pages/demo/Demo.tsx
@@ -9,8 +9,21 @@ const Demo: React.FC = () => {
   };
   return (
     <div style={{ display: 'flex', height: '88vh' }}>
-      <Button style={{ margin: 'auto' }} onClick={handleClick} type="primary" size="large">
+      <Button
+        onClick={() => Parse.User.logIn('yinqian', 'proxima0719')}
+        style={{ margin: 'auto' }}
+        type="primary"
+        size="large"
+      >
         登录
+      </Button>
+      <Button
+        onClick={() => Parse.User.logIn('onion', '123456')}
+        style={{ margin: 'auto' }}
+        type="primary"
+        size="large"
+      >
+        正常用户
       </Button>
     </div>
   );
