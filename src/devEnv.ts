42fa5730599f0ad104e648d18778a35e76beff0e
qiaochao <752716886@qq.com>
2022-01-10 18:18:07 +0800

    fix : env

--- src/devEnv.ts
+++ src/devEnv.ts
@@ -3,7 +3,7 @@ export const getDevConfig = () => {
     if (process.env.NODE_ENV === 'production' || process.env.PROXIMA_DEV_MODE === 'embed')
       return {};
     const DEV_STORAGE_KEY = 'test_manager_dev';
-    return JSON.parse(localStorage.getItem(DEV_STORAGE_KEY));
+    return JSON.parse(localStorage.getItem(DEV_STORAGE_KEY)) || {};
   } catch (err) {
     return {};
   }
