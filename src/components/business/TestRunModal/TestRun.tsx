ac0fcdc6dc3586d52f2ee3f51f5f854b2745f565
大左 <13001921389@163.com>
2022-03-15 14:05:07 +0800

    fix: 【PROXIMA-3729】测试执行页面 缺陷数项显示错误，如果事项被删除显示的关联数据个数还是删除之前的个数

--- src/components/business/TestRunModal/TestRun.tsx
+++ src/components/business/TestRunModal/TestRun.tsx
@@ -244,7 +244,7 @@ const TestRun: React.FC<TestRunType> = props => {
         return itemLinks?.length ?? 0;
       },
       defect() {
-        return allRelationDefectIds?.length ?? 0;
+        return allRelationDefectItems?.length ?? 0;
       },
     };
 
