3440c8bf5407461b14f3ec0bb51559f0873aa23c
invoker <kunpeng1992@sina.com>
2022-03-16 11:29:22 +0800

    fix(plugin-test-manager): 修改测试详情选择测试用例下拉框 bug

--- src/components/business/TestEntitySelectorModal/TestDetailSelector.tsx
+++ src/components/business/TestEntitySelectorModal/TestDetailSelector.tsx
@@ -317,6 +317,7 @@ const TestDetailSelector: React.FC<TestDetailSelectorProps> = props => {
       </div>
       <Select
         showSearch
+        getPopupContainer={trigNode => trigNode.parentElement}
         optionFilterProp="title"
         value={selectedWorkspaceKey}
         disabled={isWorkspaceIsolate}
