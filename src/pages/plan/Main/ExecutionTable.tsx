024f79fb1bc697eb230953b4439b1fc2993075b6
Juinvok <kunpeng1992@sina.com>
2022-03-18 10:44:18 +0800

    fix(plugin-test-manager): 修改测试执行任务列表数据翻页显示逻辑优化 bug

--- src/pages/plan/Main/ExecutionTable.tsx
+++ src/pages/plan/Main/ExecutionTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { notification, Pagination } from '@osui/ui';
+import { notification } from '@osui/ui';
 import { DeleteOutlined } from '@/icons';
 import { usePageContext } from '../hook';
 import { updateTestRun } from '@/lib/api/runs';
@@ -8,7 +8,6 @@ import { StatusBadge } from '@/components/business/Status';
 import { TestRelationType, TestType } from '@/lib/constants';
 import { useListener } from '@projectproxima/proxima-sdk-js';
 import { StatusProgress } from '@/components/business/Status';
-import TestRunModal from '@/components/business/TestRunModal';
 import { actionConfirm, openItemViewScreen } from '@/lib/utils/helper';
 import { addTestDetailToExecution, updateTestRunStatus } from '@/lib/api/runs';
 import {
@@ -24,7 +23,7 @@ import TestEntitySelectorModal, {
   ActionType as TestEntitySelectorActionType,
 } from '@/components/business/TestEntitySelectorModal';
 
-import cx from './executionTable.less';
+import ExpandedTable from './ExpandedTable';
 
 const ExecutionTable = () => {
   const innerTableRef = React.useRef<BusinessTableActionRef>();
@@ -253,116 +252,19 @@ const ExecutionTable = () => {
     },
   ];
 
-  const [pageNum, setPageNum] = React.useState(1);
-
   const expandedRowRender = React.useCallback(
-    record => {
-      // 测试执行序列
-      const testIdSequence = record.relRuns?.map(item => item?.objectId).filter(Boolean);
-      const handleTestRunStatusChange = async (testRunId, status) => {
-        await updateTestRun(testRunId, { status: status.key });
-        refreshAndMutateData();
-      };
-
-      const columns = [
-        {
-          key: 'detailName',
-          title: '用例标题',
-          isSystem: true,
-          fixed: true,
-          width: 160,
-          tooltip: true,
-          render(_, record) {
-            const detailItemData = record.runReferenceDetail?.reference ?? {};
-            return (
-              <span
-                style={{ cursor: 'pointer' }}
-                onClick={() => openItemViewScreen(detailItemData.objectId)}
-              >
-                {detailItemData.name}
-              </span>
-            );
-          },
-        },
-        {
-          key: 'runStatus',
-          title: '测试执行状态',
-          width: 150,
-          render(_, record) {
-            return (
-              <StatusBadge
-                useRootContainer
-                status={record.status}
-                onStatusChange={status => handleTestRunStatusChange(record.objectId, status)}
-              />
-            );
-          },
-        },
-        {
-          key: 'action',
-          title: '操作',
-          isSystem: true,
-          fixed: 'right' as any,
-          render(_, record) {
-            return (
-              <TestRunModal
-                testId={record.objectId}
-                testIdSequence={testIdSequence}
-                onCancel={() =>
-                  setTimeout(() => {
-                    refreshAndMutateData(); //刷新依赖数据
-                  }, 200)
-                }
-                trigger={<a>执行</a>}
-              />
-            );
-          },
-        },
-      ];
-
-      const PaginationFooterRender = () => {
-        return (
-          <div className={cx('footer')}>
-            <div className={cx('num')}>
-              共 <span>{record.relRuns.length}</span> 个
-            </div>
-            <Pagination
-              size="small"
-              current={pageNum}
-              defaultPageSize={10}
-              pageSizeOptions={[10]}
-              showSizeChanger={false}
-              onChange={relPageChange}
-              className={cx('pagination')}
-              total={record.relRuns.length}
-            />
-          </div>
-        );
-      };
-
-      const relPageChange = current => setPageNum(current);
-
-      return (
-        <div className={cx('expand-container')}>
-          <BusinessTable
-            className={cx('expand-table')}
-            rowKey="objectId"
-            columns={columns}
-            useColumnSetting
-            showPagination={true}
-            actionRef={innerTableRef}
-            name="ExecutionInnerTable"
-            scroll={{ x: 'max-content', y: 500 }}
-            itemKey="runReferenceDetail.reference"
-            PaginationFooterRender={PaginationFooterRender}
-            selectionActionNodes={InnerTableSelectionActionNodes}
-            onSelectionCancel={() => tableSelectionToggleEvent.emit(false)}
-            dataSource={record.relRuns.slice((pageNum - 1) * 10, pageNum * 10)}
-          />
-        </div>
-      );
-    },
-    [InnerTableSelectionActionNodes, refreshAndMutateData, tableSelectionToggleEvent, pageNum],
+    record => (
+      <ExpandedTable
+        InnerTableSelectionActionNodes={InnerTableSelectionActionNodes}
+        refreshAndMutateData={refreshAndMutateData}
+        tableSelectionToggleEvent={tableSelectionToggleEvent}
+        record={record}
+        updateTestRun={updateTestRun}
+        openItemViewScreen={openItemViewScreen}
+        innerTableRef={innerTableRef}
+      />
+    ),
+    [InnerTableSelectionActionNodes, refreshAndMutateData, tableSelectionToggleEvent],
   );
 
   return (
@@ -381,7 +283,6 @@ const ExecutionTable = () => {
         expandable={{
           expandedRowRender,
           expandRowByClick: true,
-          // rowExpandable: record => Boolean(record.relRuns.length),
         }}
         getDataSource={tableDataGetter}
         actionRef={executionTableActionRef}
---
+++ src/pages/plan/Main/ExpandedTable.tsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import { Pagination } from 'antd';
+import { StatusBadge } from '@/components/business/Status';
+import TestRunModal from '@/components/business/TestRunModal';
+import { BusinessTable } from '@/components/common/BusinessTable';
+
+import cx from './executionTable.less';
+
+interface ExpandedTableProps {
+  InnerTableSelectionActionNodes: any;
+  refreshAndMutateData: () => void;
+  tableSelectionToggleEvent: any;
+  record: any;
+  updateTestRun: any;
+  openItemViewScreen: any;
+  innerTableRef: any;
+}
+
+const ExpandedTable = (props: ExpandedTableProps) => {
+  const [pageNum, setPageNum] = React.useState(1);
+
+  const {
+    InnerTableSelectionActionNodes,
+    refreshAndMutateData,
+    tableSelectionToggleEvent,
+    record,
+    updateTestRun,
+    openItemViewScreen,
+    innerTableRef,
+  } = props;
+
+  // 测试执行序列
+  const testIdSequence = record.relRuns?.map(item => item?.objectId).filter(Boolean);
+  const handleTestRunStatusChange = async (testRunId, status) => {
+    await updateTestRun(testRunId, { status: status.key });
+    refreshAndMutateData();
+  };
+
+  const columns = [
+    {
+      key: 'detailName',
+      title: '用例标题',
+      isSystem: true,
+      fixed: true,
+      width: 160,
+      tooltip: true,
+      render(_, record) {
+        const detailItemData = record.runReferenceDetail?.reference ?? {};
+        return (
+          <span
+            style={{ cursor: 'pointer' }}
+            onClick={() => openItemViewScreen(detailItemData.objectId)}
+          >
+            {detailItemData.name}
+          </span>
+        );
+      },
+    },
+    {
+      key: 'runStatus',
+      title: '测试执行状态',
+      width: 150,
+      render(_, record) {
+        return (
+          <StatusBadge
+            useRootContainer
+            status={record.status}
+            onStatusChange={status => handleTestRunStatusChange(record.objectId, status)}
+          />
+        );
+      },
+    },
+    {
+      key: 'action',
+      title: '操作',
+      isSystem: true,
+      fixed: 'right' as any,
+      render(_, record) {
+        return (
+          <TestRunModal
+            testId={record.objectId}
+            testIdSequence={testIdSequence}
+            onCancel={() =>
+              setTimeout(() => {
+                refreshAndMutateData(); //刷新依赖数据
+              }, 200)
+            }
+            trigger={<a>执行</a>}
+          />
+        );
+      },
+    },
+  ];
+
+  const PaginationFooterRender = () => {
+    return (
+      <div className={cx('footer')}>
+        <div className={cx('num')}>
+          共 <span>{record.relRuns.length}</span> 个
+        </div>
+        <Pagination
+          size="small"
+          current={pageNum}
+          defaultPageSize={10}
+          pageSizeOptions={[10]}
+          showSizeChanger={false}
+          onChange={relPageChange}
+          className={cx('pagination')}
+          total={record.relRuns.length}
+        />
+      </div>
+    );
+  };
+
+  const relPageChange = current => setPageNum(current);
+
+  return (
+    <div className={cx('expand-container')}>
+      <BusinessTable
+        className={cx('expand-table')}
+        rowKey="objectId"
+        columns={columns}
+        useColumnSetting
+        showPagination={true}
+        actionRef={innerTableRef}
+        name="ExecutionInnerTable"
+        scroll={{ x: 'max-content', y: 500 }}
+        itemKey="runReferenceDetail.reference"
+        PaginationFooterRender={PaginationFooterRender}
+        selectionActionNodes={InnerTableSelectionActionNodes}
+        onSelectionCancel={() => tableSelectionToggleEvent.emit(false)}
+        dataSource={record.relRuns.slice((pageNum - 1) * 10, pageNum * 10)}
+      />
+    </div>
+  );
+};
+
+export default ExpandedTable;
