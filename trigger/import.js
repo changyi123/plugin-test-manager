35ddcf2497f5fccc4a4a899d7672ec3a6ba47394
Juinvok <kunpeng1992@sina.com>
2022-04-20 10:15:10 +0800

    fix(plugin-test-manager): 修改测试管理用例导入规则校验

--- trigger/import.js
+++ trigger/import.js
@@ -46,18 +46,26 @@ const getWorkspaceKey = async () => {
 
 const workspaceKey = await getWorkspaceKey();
 
-// 步骤每项的开始标志
-const stepStartToken = '【\\d+】|\\d+\\.+';
-// 步骤换行符标志
-const stepEOLToken = '\\r\\n';
-// 提取步骤 index
-const pickStepIndex = data => {
-  return data.replace(/【?(\d+)】?\.*.*?$/, '$1');
-};
-const getActionAndResultData = datas =>
-  (getCharNum(datas) > 500 ? '' : datas).replace(/^(【\d+】|\d+\.+)/, '');
+const replaceRn = datas => datas?.replace(/^[\r\n]+/g, '');
 
-const splitData = datas => datas?.split?.(/[\r\n]+/g);
+const splitSteps = datas => replaceRn(datas)?.split(/(?=【\d+】)/g) ?? [];
+
+// 步骤每项的开始标志
+// const stepStartToken = '【\\d+】|\\d+\\.+';
+const stepStartToken = '【\\d+】';
+// 步骤换行符标志
+const stepEOLToken = '[\\r\\n]';
+// 提取步骤 index
+// const pickStepIndex = data => {
+//   return +data.replace(/【?(\d+)】?\.*.*?$/, '$1');
+// };
+const pickStepIndex = data => {
+  return +data.replace(/【(\d+)】(.|[\r\n])*?$/, '$1');
+};
+// const getActionAndResultData = datas => datas.replace(/^(【\d+】|\d+\.+)/, '');
+const getStepSData = datas => datas.replace(/^【\d+】/g, '');
+
+// const splitData = datas => datas?.split?.(/[\r\n]+/g);
 
 const getStepsData = datas => {
   const stepsMap = new Map();
@@ -65,38 +73,44 @@ const getStepsData = datas => {
   const isStrictEOLModeReg = new RegExp(`(^|(${stepEOLToken}))${stepStartToken}`, 'g');
   // 严格换行模式
   const isStrictEOLMode =
-    isStrictEOLModeReg.test(datas.action) &&
-    isStrictEOLModeReg.test(datas.result) &&
-    isStrictEOLModeReg.test(datas.data);
+    isStrictEOLModeReg.test(replaceRn(datas.action)) &&
+    isStrictEOLModeReg.test(replaceRn(datas.result)) &&
+    isStrictEOLModeReg.test(replaceRn(datas.data));
 
-  splitData(datas.action ?? '')?.forEach((action, index) => {
-    stepsMap.set(isStrictEOLMode ? pickStepIndex(action) : index, {
-      action: getActionAndResultData(action),
-      result: stepsMap.get(index)?.result ?? '',
-      data: stepsMap.get(index)?.data ?? '',
-      id: stepsMap.get(index)?.id || uuidv4(),
+  splitSteps(datas.action ?? '')?.forEach((action, index) => {
+    const _index = isStrictEOLMode ? pickStepIndex(action) : index;
+    stepsMap.set(_index, {
+      action: getStepSData(action),
+      result: stepsMap.get(_index)?.result ?? '',
+      data: stepsMap.get(_index)?.data ?? '',
+      id: stepsMap.get(_index)?.id || uuidv4(),
     });
   });
 
-  splitData(datas.result ?? '')?.forEach((result, index) => {
-    stepsMap.set(isStrictEOLMode ? pickStepIndex(result) : index, {
-      action: stepsMap.get(index)?.action ?? '',
-      result: getActionAndResultData(result),
-      data: stepsMap.get(index)?.data ?? '',
-      id: stepsMap.get(index)?.id || uuidv4(),
+  splitSteps(datas.result ?? '')?.forEach((result, index) => {
+    const _index = isStrictEOLMode ? pickStepIndex(result) : index;
+    stepsMap.set(_index, {
+      action: stepsMap.get(_index)?.action ?? '',
+      result: getStepSData(result),
+      data: stepsMap.get(_index)?.data ?? '',
+      id: stepsMap.get(_index)?.id || uuidv4(),
     });
   });
 
-  splitData(datas.data ?? '')?.forEach((_data, index) => {
-    stepsMap.set(isStrictEOLMode ? pickStepIndex(_data) : index, {
-      data: getActionAndResultData(_data),
-      action: stepsMap.get(index)?.action ?? '',
-      result: stepsMap.get(index)?.result ?? '',
-      id: stepsMap.get(index)?.id || uuidv4(),
+  splitSteps(datas.data ?? '')?.forEach((_data, index) => {
+    const _index = isStrictEOLMode ? pickStepIndex(_data) : index;
+    stepsMap.set(_index, {
+      data: getStepSData(_data),
+      action: stepsMap.get(_index)?.action ?? '',
+      result: stepsMap.get(_index)?.result ?? '',
+      id: stepsMap.get(_index)?.id || uuidv4(),
     });
   });
 
-  return [...stepsMap.values()].filter(d => d.data || d.action || d.result);
+  return [...stepsMap.entries()]
+    .sort((a, b) => a[0] - b[0])
+    .map(v => v[1])
+    .filter(d => d.data || d.action || d.result);
 };
 
 // 创建测试用例数据，返回测试用例
@@ -217,7 +231,7 @@ const filterImportGroupData = datas =>
     return prev;
   }, []);
 
-const getGroupPath = group => group?.split('/').filter(d => d.replace(/\s*/g, '')) ?? [];
+const getGroupPath = group => group?.split('/').filter(d => d.trim()) ?? [];
 
 const getImportGroupData = () =>
   appFieldsData
