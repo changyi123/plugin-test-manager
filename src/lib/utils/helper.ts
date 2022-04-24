5d2960301fb35f23cd21ec60a483cb5938d6fd85
zbl91555 <zbl91555@163.com>
2022-04-24 18:10:53 +0800

    fix: 测试管理版本输出

--- src/lib/utils/helper.ts
+++ src/lib/utils/helper.ts
@@ -93,5 +93,18 @@ export const generateStaticFileUrl = (url: string) => {
   return `${/^\/project\//.test(url) ? '' : getProximaBasePath()}${url}`;
 };
 
+// 打印插件版本
+export const pluginVersionLog = () => {
+  if (process.env.NODE_ENV === 'production') {
+    // eslint-disable-next-line no-console
+    console.log('%cPLUGIN-VERSION:', 'font-size: 16px; font-weight: 700; color: skyblue');
+    // eslint-disable-next-line no-console
+    console.table({
+      Branch: (process.env as any)?.PROXIMA_BRANCH,
+      Commit: (process.env as any)?.PROXIMA_COMMIT,
+    });
+  }
+};
+
 /** panel 消息通知 */
 export { alert } from '@/components/business/PanelLayout';
