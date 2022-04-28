f57eba72d595e396468adf5a89eb39a6b1e7f3db
Juinvok <kunpeng1992@sina.com>
2022-04-28 14:40:01 +0800

    fix(pligin-test-manager): 修改前置条件、数据、步骤、等校验规则为1000 字符，修改步骤描述、预期结果等限制 1000 字符

--- src/components/business/TestStep/StepList.tsx
+++ src/components/business/TestStep/StepList.tsx
@@ -36,7 +36,7 @@ const StepFields: React.FC<{
               Object.assign(
                 {
                   key: field.key,
-                  maxLength: 500,
+                  maxLength: 1000,
                   placeholder: `请输入${field.title}（Alt+Enter切换下一列）`,
                   onKeyDownEnter: () => nextField(stepId, field.key),
                   ref: ref => {
