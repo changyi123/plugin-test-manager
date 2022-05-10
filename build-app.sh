2f3660e81d58df3ab4cdfef49bc1108afafad8a3
zbl91555 <zbl91555@163.com>
2022-05-10 15:09:38 +0800

    feat: 修改配置脚本

--- build-app.sh
+++ build-app.sh
@@ -3,6 +3,19 @@
 commit=$(git log --oneline | cut -d$'\n' -f1)
 branch=$(git rev-parse --abbrev-ref HEAD)
 
+# 浪潮环境需要隐藏掉页面
+workspacePageHidden="hidden: false"
+
+while true; do
+    read -p "是否要隐藏测试管理面板？" i
+    case $i in
+        [Yy1]* ) workspacePageHidden="hidden: true"; break;;
+        * ) break;;
+    esac
+done
+
+sed -e "s/{workspacePageHidden}/${workspacePageHidden}/g" manifest.yml.tmpl > manifest.yml
+
 yarn && yarn build -- --env PROXIMA_COMMIT="$commit" PROXIMA_BRANCH="$branch"
 rm -rf test-manager-plugin.zip
 zip -r test-manager-plugin.zip dist trigger manifest.yml
