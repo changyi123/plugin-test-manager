1c8f2dc8507d1d82af13065f7c45c61e24543759
zbl91555 <zbl91555@163.com>
2022-05-15 00:08:29 +0800

    refactor: 优化查询性能

--- src/pages/plan/Main/DetailTable.tsx
+++ src/pages/plan/Main/DetailTable.tsx
@@ -73,6 +73,8 @@ const DetailTable = () => {
           TestRelationType.PlanRelDetail,
           { from: selectedTestPlanId },
           {
+            // FIXME: 优化执行
+            workspaceKey,
             fillItemData: true,
             nameLike: searchValue,
             queryParams: queryParams,
@@ -82,6 +84,8 @@ const DetailTable = () => {
           TestRelationType.PlanRelExecution,
           { from: selectedTestPlanId },
           {
+            // FIXME: 优化执行
+            workspaceKey,
             fillItemData: true,
             queryParams: { limit: 9999 },
             include: ['objectId'],
