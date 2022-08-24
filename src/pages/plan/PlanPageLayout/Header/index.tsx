18fb4879da2bcc5b1a3acc35d3fabf3d33b00353
zbl91555 <zbl91555@163.com>
2022-08-24 16:20:56 +0800

    feat: 调整测试报告新建按钮入口

--- src/pages/plan/PlanPageLayout/Header/index.tsx
+++ src/pages/plan/PlanPageLayout/Header/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Button, notification, Spin } from 'antd';
+import { ArrowLeftOutlined, ExportOutlined } from '@/icons';
 import TestPlanSelector from '@/components/business/TestPlanSelector';
-import { ArrowLeftOutlined } from '@ant-design/icons';
 import { useBaseAction } from '@/lib/hooks/useContext';
 import { createTestExecutionAndRelations } from '@/lib/api/runs';
 import { TestType } from '@/lib/constants';
 import ExecutionList from '../ExecutionList';
 import { usePageContext } from '../../hook';
+import WordReport from '@/lib/report';
+import { getFirstWordTemplate } from '@/lib/api/report';
 
 import cx from './index.less';
 
@@ -32,6 +34,7 @@ const Header: React.FC<HeaderProps> = ({
   const { workspaceKey, selectedTestPlan, setSelectedTestPlan, tableSelectionToggleEvent } =
     usePageContext();
   const { createItemUseModal } = useBaseAction();
+  const [isReportGenerating, setIsReportGenerating] = React.useState(false);
 
   // 创建测试执行任务
   const createTestExecution = async () => {
@@ -67,6 +70,25 @@ const Header: React.FC<HeaderProps> = ({
     }
   };
 
+  const generateReport = async () => {
+    // TODO: 选取测试报告，当前只取系统第一个
+    try {
+      setIsReportGenerating(true);
+      const wordTemplate = await getFirstWordTemplate();
+      const wordTemplateGenerator = new WordReport(wordTemplate);
+      await wordTemplateGenerator.generateReport({
+        fileName: `${selectedTestPlan.reference.name}-测试报告`,
+        testPlanIds: [selectedTestPlan?.objectId],
+      });
+    } catch (err) {
+      // TODO: 错误处理
+    } finally {
+      setTimeout(() => {
+        setIsReportGenerating(false);
+      }, 200);
+    }
+  };
+
   return (
     <>
       <div className={cx('page-header')}>
@@ -94,6 +116,15 @@ const Header: React.FC<HeaderProps> = ({
               }}
             >
               测试执行任务
+            </div>
+            <div className={cx('tab-extra-action')}>
+              <Button
+                onClick={generateReport}
+                icon={<ExportOutlined />}
+                loading={isReportGenerating}
+              >
+                生成测试报告
+              </Button>
             </div>
           </div>
         </div>
