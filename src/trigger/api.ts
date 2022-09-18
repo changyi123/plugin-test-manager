8a166435de1d133ab3875c15a0bc0889abf05861
zbl91555 <zbl91555@163.com>
2022-09-18 20:20:02 +0800

    feat: 新增测试执行统计接口

--- src/trigger/api.ts
+++ src/trigger/api.ts
@@ -1,3 +1,5 @@
 export { queryTestEntity, queryLinkedTestEntity } from './modules/api/query';
 
 export { batchDelete, batchUpdate, batchCreateTestRun } from './modules/api/batch';
+
+export { testPlanStats } from './modules/api/stats';
