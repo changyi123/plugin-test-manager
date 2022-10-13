06072501b1a0a898c257060e160bfc67f374e744
wush <30492640+wush12@users.noreply.github.com>
2022-10-13 18:05:48 +0800

    add isInOne

--- src/app/lib/utils/helper.ts
+++ src/app/lib/utils/helper.ts
@@ -17,7 +17,7 @@ export const getProximaBasePath = () => {
 // 获取 webTrigger 前缀
 export const getPluginWebTriggerBaseUrl = () => {
   // 集成环境需要先判断前缀
-  const ApiPrefix = inIframe() ? getProximaBasePath() : '';
+  const ApiPrefix = isInOne() ? getProximaBasePath() : '';
   return `/api${ApiPrefix}/app/${getTenantKey()}/test_manager/webhooks`;
 };
 
@@ -118,10 +118,23 @@ export const inIframe = (): boolean => {
 
   if (inServer) return false;
   try {
+    return window.self !== window.top;
+  } catch (e) {
+    console.info('inIframe', e);
+    return true;
+  }
+};
+
+export const isInOne = () => {
+  const isServer = (): boolean => typeof window === 'undefined';
+  const inServer = isServer();
+
+  if (inServer) return false;
+  try {
     const gateway = (window as any).env.PROXIMA_GATEWAY;
     return startsWith(new URL(gateway).pathname, '/api');
   } catch (e) {
-    console.info('inIframe', e);
+    console.info('isInOne', e);
     return true;
   }
 };
