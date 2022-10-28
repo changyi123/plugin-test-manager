178fb907f999081ea2aec9faef9f7099f4a4dfce
Juinvok <kunpeng1992@sina.com>
2022-10-28 11:17:09 +0800

    fix: 修改集成环境bug

--- src/app/components/business/TestManagerProvider/index.tsx
+++ src/app/components/business/TestManagerProvider/index.tsx
@@ -120,10 +120,18 @@ const getOrBatchCreateTestEntities = async (
 
     const curStore = list[index]?.[CREATE_ITEM_STORE_FIELD_KEY];
 
-    if (curStore?.repository) {
-      return {
-        repository: curStore?.repository,
-      };
+    if (curStore) {
+      if (curStore.repository) {
+        return {
+          repository: curStore?.repository,
+        };
+      }
+
+      if (curStore.repository === null) {
+        return {
+          repository: curStore?.repository,
+        };
+      }
     }
 
     const newList = list.slice(0, index).reverse();
