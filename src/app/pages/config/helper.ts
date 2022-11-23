f8bf6eb91b6f251ee7e9d77e4388a8a42ba012f7
Juinvok <kunpeng1992@sina.com>
2022-11-23 16:04:30 +0800

    fix: 注释缺陷面板

--- src/app/pages/config/helper.ts
+++ src/app/pages/config/helper.ts
@@ -1,27 +1,27 @@
 import { TestType, BuiltinItemTypeMapping } from '@/lib/constants';
-import { Board, Workspace } from '@/lib/models';
+// import { Board, Workspace } from '@/lib/models';
 
 /** 获取默认测试配置数据 */
 export const generateDefaultTestConfig = async (
   workspace: Record<string, any>,
   isolatedSystem = false,
 ) => {
-  const board = await new Board({
-    // 空间内面板
-    filterSource: 'inWorkspace',
-    icon: 'Panel1',
-    iql: '\'类型\' in ["缺陷"]',
-    name: '缺陷管理',
-    // 限制创建的类型
-    itemTypes: [],
-    // 面板所属空间
-    workspace: Workspace.createWithoutData(workspace.objectId),
-    hidden: true,
-  }).save();
+  // const board = await new Board({
+  //   // 空间内面板
+  //   filterSource: 'inWorkspace',
+  //   icon: 'Panel1',
+  //   iql: '\'类型\' in ["缺陷"]',
+  //   name: '缺陷管理',
+  //   // 限制创建的类型
+  //   itemTypes: [],
+  //   // 面板所属空间
+  //   workspace: Workspace.createWithoutData(workspace.objectId),
+  //   hidden: true,
+  // }).save();
 
   return {
-    displayDefectBoard: true,
-    defectBoard: board,
+    // displayDefectBoard: true,
+    // defectBoard: board,
     workspaceKey: workspace.key,
     global: false,
     itemTypeMap: isolatedSystem ? BuiltinItemTypeMapping : {},
--- src/trigger/modules/web/script/gitee-menus.ts
+++ src/trigger/modules/web/script/gitee-menus.ts
@@ -1,4 +1,4 @@
-import { TestConfigClassName } from '../../../../common/constant';
+// import { TestConfigClassName } from '../../../../common/constant';
 import { getParseQuery, getAppsData } from '@giteeteam/apps-team-api';
 
 const log = (msg, ...restArgs) => {
@@ -26,7 +26,7 @@ export const runGiteeMenus = async () => {
     sessionToken: global.sessionToken,
   };
 
-  const getGiteeMenusConfig = async (appId, workspaceKey) => {
+  const getGiteeMenusConfig = (appId, workspaceKey) => {
     const tenantKey = getTenantKey();
     const productPrefix = getProductPrefix();
 
@@ -47,36 +47,36 @@ export const runGiteeMenus = async () => {
     };
 
     // 获取测试管理空间配置
-    const testConfigQuery = getParseQuery(false, TestConfigClassName);
-    const testConfig = await testConfigQuery
-      .equalTo('workspaceKey', workspaceKey)
-      .select(['defectBoard', 'displayDefectBoard'])
-      .include('defectBoard')
-      .first(ParseBaseQueryOptions)
-      .then(item => item.toJSON());
+    // const testConfigQuery = getParseQuery(false, TestConfigClassName);
+    // const testConfig = await testConfigQuery
+    //   .equalTo('workspaceKey', workspaceKey)
+    //   .select(['defectBoard', 'displayDefectBoard'])
+    //   .include('defectBoard')
+    //   .first(ParseBaseQueryOptions)
+    //   .then(item => item.toJSON());
 
-    const boardMenus = [];
+    // const boardMenus = [];
 
-    if (testConfig?.defectBoard) {
-      const defectBoardData = testConfig?.defectBoard;
+    // if (testConfig?.defectBoard) {
+    //   const defectBoardData = testConfig?.defectBoard;
 
-      boardMenus.push({
-        title: '缺陷管理',
-        key: `${defectBoardData.key}-${workspaceKey}`,
-        icon: 'iconNavi-icafeplan',
-        url: `${giteeRoutePrefix}/boards/${defectBoardData.key}`,
-        type: 'IFRAME',
-        openWindow: '0',
-        iframeUrl: `${proximaRoutePrefix}/boards/${defectBoardData.key}?hiddenSider=true&hiddenHeader=true`,
-      });
-    }
+    //   boardMenus.push({
+    //     title: '缺陷管理',
+    //     key: `${defectBoardData.key}-${workspaceKey}`,
+    //     icon: 'iconNavi-icafeplan',
+    //     url: `${giteeRoutePrefix}/boards/${defectBoardData.key}`,
+    //     type: 'IFRAME',
+    //     openWindow: '0',
+    //     iframeUrl: `${proximaRoutePrefix}/boards/${defectBoardData.key}?hiddenSider=true&hiddenHeader=true`,
+    //   });
+    // }
 
     const menus = [
       { name: '测试计划', pageKey: 'test-plan' },
       { name: '测试用例库', pageKey: 'test-repository' },
     ].map(generateGiteeMenu);
 
-    return menus.concat(boardMenus);
+    return menus;
   };
 
   // const appQuery = getParseQuery(false, 'App');
