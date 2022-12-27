bb55a148855e2601752a4c31afff62f48c26cdbd
zbl91555 <zbl91555@163.com>
2022-12-27 18:50:30 +0800

    feat: 导航菜单增加国际化

---
+++ src/lang/zh.ts
@@ -0,0 +1,7 @@
+export default {
+  navigation: {
+    repository: '测试用例',
+    plan: '测试计划',
+    overview: '测试概览',
+  },
+};
---
+++ src/trigger/lib/lang.ts
@@ -0,0 +1,24 @@
+import en from '../../lang/en';
+import zh from '../../lang/zh';
+import get from 'lodash/get';
+
+export const getLang = (defaultLang?: string) => {
+  return global.headers.lang ?? defaultLang ?? 'en-US';
+};
+
+export const getLangPkg = (defaultLang?: string) => {
+  const lang = getLang(defaultLang);
+  console.info('lang------------>', lang);
+  switch (lang) {
+    case 'zh-CN':
+      return zh;
+    case 'en-US':
+    default:
+      return en;
+  }
+};
+
+export const t = key => {
+  const langPkg = getLangPkg();
+  return get(langPkg, key);
+};
