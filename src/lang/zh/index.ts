b87ce2b7a34daacda54949369bb081e8c91e90c7
Juinvok <kunpeng1992@sina.com>
2023-01-05 17:01:28 +0800

    feat: 添加国家化问题件

---
+++ src/lang/zh/index.ts
@@ -0,0 +1,11 @@
+import importer from './importer';
+import report from './report';
+import script from './script';
+import trigger from './trigger';
+
+export default {
+  importer,
+  report,
+  script,
+  trigger,
+};
