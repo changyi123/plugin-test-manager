91c358315c695777b59c879168796b42038ae97e
Juinvok <kunpeng1992@sina.com>
2023-02-07 11:06:13 +0800

    fix: 修改查询缺陷 iql

--- src/app/components/business/TestEntitySelectorModal/index.tsx
+++ src/app/components/business/TestEntitySelectorModal/index.tsx
@@ -183,13 +183,14 @@ const TestEntitySelector: React.FC<TestEntitySelectorProps> = props => {
   // 获取测试用例
   const { runAsync: getTestEntityByKeyword, loading: searchLoading } = useRequest(
     async keyword => {
+      const params = testType === TestType.TestDefect ? {} : { r_test_manager_type: testType };
       const { items } = await getItemByIQL({
         limit: 50,
         nameOrKeyLike: keyword,
         itemType: itemTypeCondition,
         orderBy: ['修改时间', 'desc'],
         workspace: workspaceKeyCondition,
-        r_test_manager_type: testType,
+        ...params,
       });
 
       const itemDict = keyBy(items, 'objectId');
@@ -220,6 +221,7 @@ const TestEntitySelector: React.FC<TestEntitySelectorProps> = props => {
       });
     },
     {
+      refreshDeps: [testType],
       manual: true,
     },
   );
