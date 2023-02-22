00f2014b9d4b4bc62280c9836e492be113c38ec6
zbl91555 <zbl91555@163.com>
2023-02-22 16:59:43 +0800

    fix: 修复脑图国际化问题

--- src/trigger/modules/api/module.ts
+++ src/trigger/modules/api/module.ts
@@ -1,7 +1,7 @@
 import keyBy from 'lodash/keyBy';
 import { logTimeCost } from '../../lib/logger';
 import { iqlRequest } from '../../lib/iqlRequest';
-import { getParseQuery } from '@giteeteam/apps-team-api';
+import { getParseQuery, i18n } from '@giteeteam/apps-team-api';
 import { getReqInfoFromVMRuntime, buildResponse } from '../../lib/apiUtil';
 import { RepositoryTreePayload, MinderDataPayload } from '../../../common/types/api';
 import {
@@ -32,7 +32,7 @@ const getRepositoryTree = async ({ workspaceKey, sessionToken, select = [] }) =>
 
     const ungroupedRepository = {
       key: UngroupedRepositoryKey,
-      name: '全部用例',
+      name: i18n.t('common.minderRootNodeName'),
       parentKey: null,
       caseIds: [],
     };
