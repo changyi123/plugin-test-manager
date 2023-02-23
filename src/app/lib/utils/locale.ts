ee0f5bc3fa6d54eb5a2d194dfcc981b3ddb16eba
zbl91555 <zbl91555@163.com>
2023-02-23 17:40:38 +0800

    feat: 接口请求增加 accept-header

--- src/app/lib/utils/locale.ts
+++ src/app/lib/utils/locale.ts
@@ -1,4 +1,5 @@
 import resource from '../../../../locales';
+import { get } from 'lodash';
 
 export function getMessages(
   locales: string | string[] = ['zh'],
@@ -37,4 +38,49 @@ export function getMessages(
     return ['en', resource.en || {}, import('antd/lib/locale/en_US')];
   }
   return [locale, langBundle, antdLang];
+}
+
+export function getLocales(defaultLang = 'en'): string | string[] {
+  let lang = defaultLang;
+  const storageLangValue = localStorage.getItem('lang');
+
+  try {
+    if (process.env.NODE_ENV === 'production' && window.__POWERED_BY_QIANKUN__) {
+      lang = get(
+        window.QiankunProps?.Parse?.CoreManager?.get('REQUEST_HEADERS'),
+        'Accept-Language',
+      );
+    } else if (storageLangValue) {
+      lang = genAcceptLanguage(
+        typeof storageLangValue === 'string' && storageLangValue.startsWith('"')
+          ? JSON.parse(storageLangValue)
+          : storageLangValue,
+      );
+    } else if (window.QiankunProps?.context?.env?.LOCALES) {
+      lang = genAcceptLanguage(window.QiankunProps.context.env.LOCALES);
+    }
+  } catch (err) {
+    lang = defaultLang;
+  }
+
+  return lang;
+}
+
+// 获取请求头
+export function genAcceptLanguage(lang) {
+  switch (lang) {
+    case 'zh-CN':
+      return 'zh-CN;q=0.9,zh;q=0.8,en;q=0.7';
+    case 'zh':
+      return 'zh;q=0.8,en;q=0.7';
+    case 'ru-RU':
+      return 'ru-RU;q=0.9,ru;q=0.8,en;q=0.7';
+    case 'ru':
+      return 'ru;q=0.8,en;q=0.7';
+    case 'en-US':
+      return 'en-US,en;q=0.9';
+    case 'en':
+    default:
+      return 'en;q=0.9';
+  }
 }
