84743ad8afc339aa362f8cec61cf69ceb2a05736
Juinvok <kunpeng1992@sina.com>
2023-03-13 11:17:48 +0800

    fix: [proxima-10899]修改继续创建下一个测试执行任务，最后创建的一个显示不出来，需要手动刷新页面才能显示 bug

--- src/app/pages/plan/PlanPageLayout/index.tsx
+++ src/app/pages/plan/PlanPageLayout/index.tsx
@@ -310,6 +310,9 @@ const PlanPageLayout: React.FC<any> = () => {
             onCancel={() => {
               refresh();
             }}
+            afterClose={() => {
+              refresh();
+            }}
             planId={selectedTestPlan?.objectId}
           />
         </>
