33865217c53c6c6210f6399db194adc67097ed33
Juinvok <kunpeng1992@sina.com>
2023-06-09 13:56:22 +0800

    fix: [-]添加必填校验

--- src/app/pages/report/Steps/RangeForm.tsx
+++ src/app/pages/report/Steps/RangeForm.tsx
@@ -172,7 +172,7 @@ const RangeForm: React.FC<any> = ({ state, workspace }) => {
   return (
     <>
       <div className={cx('form-box')}>
-        <span className={cx('step-label')}>{t('report.range')}</span>
+        <span className={cx('step-label', 'required')}>{t('report.range')}</span>
         <div className={cx('step-cont')}>
           {currentSelector
             ?.filter(item => item?.fieldId !== 'name')
