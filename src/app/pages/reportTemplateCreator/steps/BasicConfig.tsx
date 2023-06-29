80e11ed849d895d7b500a23f1cd1288455b2e019
zbl91555 <zbl91555@163.com>
2023-06-29 16:30:21 +0800

    fix: [--] 修复国际化文案

--- src/app/pages/reportTemplateCreator/steps/BasicConfig.tsx
+++ src/app/pages/reportTemplateCreator/steps/BasicConfig.tsx
@@ -86,7 +86,13 @@ const BasicConfig: React.FC<{
           <p className={cx('label', 'required')}>{scopedT('form.name.label')}</p>
           <Form.Item
             name="name"
-            rules={[{ required: true, message: scopedT('form.name.error') }]}
+            rules={[
+              { required: true, message: scopedT('form.name.error') },
+              {
+                max: 25,
+                message: scopedT('form.name.error'),
+              },
+            ]}
             noStyle
           >
             <Input
