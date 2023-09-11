e0a4407efeeff45cee79a18c52236606d4653c91
zbl91555 <zbl91555@163.com>
2023-09-11 16:36:15 +0800

    fix: [--] 修复 lint 错误

--- src/app/services/key.ts
+++ src/app/services/key.ts
@@ -1,6 +1,3 @@
-export { CommonQueryKeys } from './common/query';
 export { TestConfigQueryKeys } from './testConfig/query';
 export { TestReportQueryKeys } from './testReport/query';
 export { CommonQueryKeys } from './common/query';
-
-export type { CommonQueryKeysType } from './common/query';
