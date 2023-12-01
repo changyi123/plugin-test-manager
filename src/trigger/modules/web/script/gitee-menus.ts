43e75c9221e0442772d30616f958fcf0e60532cd
wang <245234166@qq.com>
2023-12-01 11:06:38 +0800

    feat: [proxima-19522] 自定义设置测试管理菜单权限

--- src/trigger/modules/web/script/gitee-menus.ts
+++ src/trigger/modules/web/script/gitee-menus.ts
@@ -1,5 +1,5 @@
 import { i18n } from '@giteeteam/apps-api';
-import { getParseQuery } from '@giteeteam/apps-team-api';
+import { getAppsData, getParseQuery } from '@giteeteam/apps-team-api';
 
 const log = (msg, ...restArgs) => {
   console.info(`[testManager] ${msg}`, ...restArgs);
@@ -18,6 +18,18 @@ const getProductPrefix = () => {
   return global.headers['x-proxima-prefix'] ?? 'project';
 };
 
+// 获取pluginKey
+const getBoardPluginKey = (key, appInstance) => {
+  return `${appInstance.key}_${appInstance.objectId}_${key}`;
+};
+
+const MENU_MAP = {
+  TEST_PLAN: 'test-plan', // 测试计划
+  TEST_REPORT: 'test-report', // 测试报告
+  TEST_REPOSITORY: 'test-repository', // 测试用例库
+  TEST_STATS_REPORT: 'test-stats-report', // 测试统计
+};
+
 export const runGiteeMenus = async () => {
   log('gitee-menus webTrigger start');
   log(global);
@@ -28,7 +40,7 @@ export const runGiteeMenus = async () => {
     useMasterKey: true,
   };
 
-  const getGiteeMenusConfig = async (workspaceKey, info) => {
+  const getGiteeMenusConfig = async (workspaceKey, info, appInstance) => {
     const tenantKey = getTenantKey();
     const productPrefix = getProductPrefix();
 
@@ -39,6 +51,7 @@ export const runGiteeMenus = async () => {
     const generateGiteeMenu = ({ langKey, pageKey }) => {
       return {
         title: i18n.t(`common.pageTitle.${langKey}`),
+        pageKey,
         key: `${pageKey}-${workspaceKey}`,
         icon: 'iconNavi-icafeplan',
         // url: `${giteeRoutePrefix}/plugin/${APP_KEY}_${appId}_${pageKey}`,
@@ -52,7 +65,8 @@ export const runGiteeMenus = async () => {
 
     const reportStatsMenu = {
       title: i18n.t('common.overview'),
-      key: `test-stats-report-${workspaceKey}`,
+      pageKey: MENU_MAP.TEST_STATS_REPORT,
+      key: `${MENU_MAP.TEST_STATS_REPORT}-${workspaceKey}`,
       icon: 'iconNavi-icafeplan',
       url: `${giteeRoutePrefix}/plugin/team_insight_charts_base_team_insight_charts_base_workspace?disabledCreate=true&displayContext=test_manager&moduleKey=test_manager`,
       type: 'IFRAME',
@@ -65,15 +79,34 @@ export const runGiteeMenus = async () => {
     const enableTestReport = global.env?.FEATURE_FLAGS?.includes('ENABLE_TEST_REPORT');
 
     const menus = [
-      { pageKey: 'test-plan', langKey: 'plan' },
-      { pageKey: 'test-repository', langKey: 'repository' },
-      enableTestReport && { pageKey: 'test-report', langKey: 'report' },
+      { pageKey: MENU_MAP.TEST_PLAN, langKey: 'plan' },
+      { pageKey: MENU_MAP.TEST_REPOSITORY, langKey: 'repository' },
+      enableTestReport && { pageKey: MENU_MAP.TEST_REPORT, langKey: 'report' },
     ]
       .filter(Boolean)
       .map(generateGiteeMenu)
       .concat(reportStatsMenu);
 
-    return menus;
+    const pluginKeys = Object.keys(MENU_MAP).map(key =>
+      getBoardPluginKey(MENU_MAP[key], appInstance),
+    );
+
+    // 查询当前用户有权限访问的Board
+    const hasPermissionBoards = await getParseQuery(false, 'Board')
+      .containedIn('pluginKey', pluginKeys)
+      .find({ sessionToken: global.sessionToken })
+      .then(boards => boards?.map(board => board?.get('pluginKey')));
+
+    console.info(
+      '------------------------hasPermissionBoards------------------------------------',
+      JSON.stringify(hasPermissionBoards),
+    );
+
+    return menus.filter(menu => {
+      const pluginKey = getBoardPluginKey(menu.pageKey, appInstance);
+      console.info('pluginKey', pluginKey);
+      return hasPermissionBoards.findIndex(key => key === pluginKey) > -1;
+    });
   };
 
   const workspaceKey = getWorkspaceKey();
@@ -82,17 +115,25 @@ export const runGiteeMenus = async () => {
     if (!workspaceKey) throw new Error('NO_WORKSPACE_KEY');
     console.info(
       'test_manager_global.env.ENABLE_PLUGIN_WORKSPACE_KEY',
-      global.env.ENABLE_PLUGIN_WORKSPACE_KEY,
+      global.env?.ENABLE_PLUGIN_WORKSPACE_KEY,
     );
+
+    // 查询插件信息
+    const appInstance = await getAppsData('Apps', { key: APP_KEY });
+
     // 判断环境变量中是否有 ENABLE_PLUGIN_WORKSPACE_KEY, 如果有则直接对比判断，不走应用中心表查询
     if (Array.isArray(global?.env?.ENABLE_PLUGIN_WORKSPACE_KEY)) {
       const isEnabled = global.env.ENABLE_PLUGIN_WORKSPACE_KEY.includes(workspaceKey);
       if (isEnabled) {
-        return getGiteeMenusConfig(workspaceKey, {
-          token: 'plugin-config',
+        return getGiteeMenusConfig(
           workspaceKey,
-          pluginConfig: global?.env?.ENABLE_PLUGIN_WORKSPACE_KEY,
-        });
+          {
+            token: 'plugin-config',
+            workspaceKey,
+            pluginConfig: global?.env?.ENABLE_PLUGIN_WORKSPACE_KEY,
+          },
+          appInstance,
+        );
       }
     }
 
@@ -123,12 +164,13 @@ export const runGiteeMenus = async () => {
           ?.get('workspaces')
           ?.map(workspace => workspace.get?.('key') ?? workspace?.key ?? workspace),
         workspaceKey,
+        appInstance,
       };
 
       if (!hasTestManagerPlugin) throw new Error('CURRENT_WORKSPACE_NOT_TEST_INSTALLED');
     }
 
-    return getGiteeMenusConfig(workspaceKey, info);
+    return getGiteeMenusConfig(workspaceKey, info, appInstance);
   };
 
   try {
