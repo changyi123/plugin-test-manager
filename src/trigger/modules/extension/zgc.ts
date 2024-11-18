67f6f4a2c0ec9b3b235c604a11cb982af51cc8e0
qinhaotian <qinhaotian@oschina.cn>
2024-11-18 11:46:44 +0800

    feat: [Proxima-1116] 中关村导出word更改

--- src/trigger/modules/extension/zgc.ts
+++ src/trigger/modules/extension/zgc.ts
@@ -213,6 +213,7 @@ export const zgcTestReportInfo = async () => {
         });
 
         setRes({ storyCount: storyList.length || 0 });
+        setRes({ storyList });
       });
       await Promise.all([requestTestList, requestTestPlanList, requestReportList, requestRunList]);
       console.info('zgc', JSON.stringify({ res, groupMap }));
@@ -223,32 +224,21 @@ export const zgcTestReportInfo = async () => {
       setRes({ testCoverage: testCoverage.toFixed(2) });
 
       const getEnvList = async () => {
-        const envMap = await search(`id in ${JSON.stringify(envIds.filter(Boolean))}`, [
-          'name',
-          'id',
-        ]).then(data => {
-          return data.reduce((map, env) => ({ ...map, [env.id]: env.name }), {});
+        const envList = [];
+        Object.entries(groupMap).map(([test_time, testList]) => {
+          const existed = envList.find(env => env.test_time === test_time);
+          if (!existed) {
+            envList.push({
+              test_time,
+              workspaceName: testList[0].workspace.name,
+              test_env: '',
+              env_desc: '',
+            });
+          }
         });
-        const envList = Object.entries(groupMap).map(([test_time, testList]) => ({
-          test_time,
-          workspaceName: testList[0].workspace.name,
-          test_env: testList
-            .map(test => envMap[test.values?.[zgcConfig.测试环境 ?? 'bchj']?.[0]])
-            .filter(Boolean)
-            .join(','),
-          env_desc: uniq(
-            testList
-              .map(test => {
-                const story = planToStoryMap[test.values[TestFiledKeyMapping.linkItems]?.[0]];
-                const env = envMap[test.values[zgcConfig.测试环境 ?? 'bchj']?.[0]];
-                return `需求${story ?? ''} 在${env ?? ' '} 环境进行测试`;
-              })
-              .filter(Boolean),
-          ).join(','),
-        }));
 
         const columns = [
-          { title: '所属空间', dataIndex: 'workspaceName' },
+          { title: '系统名称', dataIndex: 'workspaceName' },
           { title: '测试阶段', dataIndex: 'test_time' },
           { title: '测试环境', dataIndex: 'test_env' },
           { title: '需求环境说明', dataIndex: 'env_desc' },
@@ -269,10 +259,10 @@ export const zgcTestReportInfo = async () => {
           ).join(','),
         }));
         const columns = [
-          { title: '所属空间', dataIndex: 'workspaceName' },
+          { title: '系统名称', dataIndex: 'workspaceName' },
           { title: '测试阶段', dataIndex: 'test_time' },
-          { title: '版本号', dataIndex: 'test_version' },
-          { title: '版本下载地址', dataIndex: 'test_version_url' },
+          { title: '被测版本号', dataIndex: 'test_version' },
+          { title: '被测版本下载地址', dataIndex: 'test_version_url' },
         ];
         setRes({ ['被测系统版本']: createTable(columns, versionList, 'version') });
       };
