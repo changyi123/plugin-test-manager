390a1582f7b5da2630283e9799a02c6a14f79e08
yaohengyu <yaohengyu@oschina.cn>
2025-04-10 17:08:18 +0800

    fix: [proxima-31940] 从其他空间导入，导入选择用例弹出，出现用例没加载出来的情况

--- src/app/components/business/TestEntitySelectorModal/TestDetailsSelectorList.tsx
+++ src/app/components/business/TestEntitySelectorModal/TestDetailsSelectorList.tsx
@@ -426,6 +426,7 @@ const TestDetailsSelectorList: React.FC<TestDetailsSelectorListProps> = ({
           {showList ? (
             <VirtualScrollList
               group={group}
+              loading={testCaseListLoading}
               caseListMap={treeType === 'plan' ? planCaseListMap : caseListMap}
               selectCaseIdsSet={selectCaseIdsSet}
               setSelectCaseIdsSet={setSelectCaseIdsSet}
