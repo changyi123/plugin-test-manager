477bb48f4d5fd9c2d7bed0a07b93d07c43bf0b94
xuxiaoxiong <xuxiaoxiong>
2025-06-25 18:54:58 +0800

    feat: [proxima-33667] 用例集key名字修改

--- src/trigger/modules/web/script/gitee-menus.ts
+++ src/trigger/modules/web/script/gitee-menus.ts
@@ -91,14 +91,16 @@ export const runGiteeMenus = async () => {
       { pageKey: MENU_MAP.TEST_PLAN, langKey: 'plan' },
       { pageKey: MENU_MAP.TEST_TASK, langKey: 'task' },
       { pageKey: MENU_MAP.TEST_REPOSITORY, langKey: 'repository' },
-      { pageKey: MENU_MAP.TEST_CASE_SET, langKey: 'testcaseset' },
+      { pageKey: MENU_MAP.TEST_CASE_SET, langKey: 'testCaseSet' },
       enableTestReport && { pageKey: MENU_MAP.TEST_REPORT, langKey: 'report' },
     ]
       .filter(Boolean)
       .map(generateGiteeMenu)
       .concat(reportStatsMenu);
 
-    const pluginKeys = Object.keys(MENU_MAP).map(key => getBoardPluginKey(APP_KEY, MENU_MAP[key])).concat(`${APP_KEY}_${MENU_MAP.TEST_STATS_REPORT}`);
+    const pluginKeys = Object.keys(MENU_MAP)
+      .map(key => getBoardPluginKey(APP_KEY, MENU_MAP[key]))
+      .concat(`${APP_KEY}_${MENU_MAP.TEST_STATS_REPORT}`);
 
     console.info('----------pluginKeys------', JSON.stringify(pluginKeys));
 
@@ -108,7 +110,7 @@ export const runGiteeMenus = async () => {
     console.info('----userSessionToken', userSessionToken);
 
     // 查询当前用户有权限访问的Board
-    let hasPermissionBoards = await getParseQuery(false, 'Board')
+    const hasPermissionBoards = await getParseQuery(false, 'Board')
       .containedIn('pluginKey', pluginKeys)
       .matchesQuery('workspace', getParseQuery(false, 'Workspace').equalTo('key', workspaceKey))
       .find({ sessionToken: userSessionToken })
@@ -125,7 +127,10 @@ export const runGiteeMenus = async () => {
         console.info('pluginKey', pluginKey);
         // 测试概览需要特殊处理，如果没有测试概览的权限，移除仪表盘的菜单
         if (pluginKey === getBoardPluginKey(APP_KEY, MENU_MAP.TEST_STATS_REPORT)) {
-          return hasPermissionBoards.includes(`${APP_KEY}_${MENU_MAP.TEST_STATS_REPORT}`) && hasPermissionBoards.findIndex(key => key === pluginKey) > -1;
+          return (
+            hasPermissionBoards.includes(`${APP_KEY}_${MENU_MAP.TEST_STATS_REPORT}`) &&
+            hasPermissionBoards.findIndex(key => key === pluginKey) > -1
+          );
         }
         return hasPermissionBoards.findIndex(key => key === pluginKey) > -1;
       })
