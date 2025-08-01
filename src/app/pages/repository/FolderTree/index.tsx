5ac8f34290c5c87a9795f90c7c8589fa3dd30346
wang <245234166@qq.com>
2025-08-01 13:26:11 +0800

    feat: [proxima-35385] 用例导入后支持打版本

--- src/app/pages/repository/FolderTree/index.tsx
+++ src/app/pages/repository/FolderTree/index.tsx
@@ -476,13 +476,18 @@ const FolderTree: React.FC<FolderTreeProps> = ({
       } else if (actionKey === MenuKey.importTest) {
         // iframe 中跳转链接增加隐藏 header 和 sider 属性
         const appendedQueryString = inIframe() ? '&hiddenSider=true&hiddenHeader=true' : '';
+        const isEnableImportSnapShotConfig = getAppEnv('ENABLE_IMPORT_SNAP_SHOT_CONFIG');
+
+        const showSnapShotConfigString = isEnableImportSnapShotConfig
+          ? '&showSnapShotConfig=true'
+          : '';
         const baseUrl = getProximaBasePath() ? `${getProximaBasePath()}` : '/';
         // 跳转到导入页面
         const href = `${baseUrl}/${getTenantKey()}/workspaces/${workspace.key}/import/${
           workspace.objectId
         }?app=test_manager&disableToggleWorkspace=true&hiddenItemType=true${appendedQueryString}&group=${
           node.key
-        }&validateRequired=${getAppEnv('GROUP_REQUIRED_WHEN_VALIDATE')}`;
+        }&validateRequired=${getAppEnv('GROUP_REQUIRED_WHEN_VALIDATE')}${showSnapShotConfigString}`;
         window.open(href);
       }
 
