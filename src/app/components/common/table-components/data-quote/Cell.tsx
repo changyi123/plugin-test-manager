7eae8273b9fcb3775fa149a5c4ad48e0c2ab2f47
xuxiaoxiong <xuxiaoxiong>
2025-08-14 10:27:46 +0800

    fix: [proxima-35386] 版本管理删除功能修改

--- src/app/components/common/table-components/data-quote/Cell.tsx
+++ src/app/components/common/table-components/data-quote/Cell.tsx
@@ -10,6 +10,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import useCellComponent from '../useCellComponent';
 import DataQuoteActionCell from './ActionCell';
 import cx from './index.module.less';
+import { dataQuoteInit } from './util';
 
 const DataQuoteCell = React.forwardRef((props: Record<string, any>, ref) => {
   const { overlayClsName, value, text, userData, property } = props;
